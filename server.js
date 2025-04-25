65468e61df2fdd4b3491c3d8fdddf20b5206dcd7
Ola Ogunyemi <ooguny04@student.bbk.ac.uk>
2025-04-25 18:30:05 +0100

    header changes to urls

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://staff.bluebirdschildcare.co.uk');
+        const allowedOrigins = ['http://localhost:5173', 'https://staff.bluebirdschildcare.co.uk'];
+        const origin = req.headers.origin;
+        if (allowedOrigins.includes(origin)) {
+             res.setHeader('Access-Control-Allow-Origin', origin);
+        }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
