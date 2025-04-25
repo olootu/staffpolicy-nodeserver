ffd007b0d98982816ca1dfb7b916a9aa110208fa
Ola Ogunyemi <ooguny04@student.bbk.ac.uk>
2025-04-25 18:35:48 +0100

    another header

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+    return next();
 });
 
 // ✅ Use a pool instead of a single connection
